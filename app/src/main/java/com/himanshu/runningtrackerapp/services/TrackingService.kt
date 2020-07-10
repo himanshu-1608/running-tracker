bcde92714578cc9d942dbfb6773bd94a862e0b65
Himanshu Yadav <ydv1608@gmail.com>
2020-07-11 03:41:32 +0530

    Added Tracking Services

--- app/src/main/java/com/himanshu/runningtrackerapp/services/TrackingService.kt
+++ app/src/main/java/com/himanshu/runningtrackerapp/services/TrackingService.kt
@@ -1,18 +1,56 @@
 package com.himanshu.runningtrackerapp.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.maps.model.LatLng
+import com.himanshu.runningtrackerapp.R
 import com.himanshu.runningtrackerapp.other.Constants.ACTION_PAUSE_SERVICE
+import com.himanshu.runningtrackerapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.himanshu.runningtrackerapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.himanshu.runningtrackerapp.other.Constants.ACTION_STOP_SERVICE
+import com.himanshu.runningtrackerapp.other.Constants.NOTIFICATION_CHANNEL_ID
+import com.himanshu.runningtrackerapp.other.Constants.NOTIFICATION_CHANNEL_NAME
+import com.himanshu.runningtrackerapp.other.Constants.NOTIFICATION_ID
+import com.himanshu.runningtrackerapp.ui.MainActivity
+import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
+
+    var isFirstRun = true
+
+    companion object {
+        val isTracking  = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-
+                    if(isFirstRun) {
+                        startForegroundService()
+                        isFirstRun = false
+                    } else {
+                        Timber.d("Resuming Service...")
+                    }
                 }
                 ACTION_PAUSE_SERVICE -> {
 
@@ -24,5 +62,51 @@ class TrackingService : LifecycleService() {
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+    private fun startForegroundService() {
+        addEmptyPolyline()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this,MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
